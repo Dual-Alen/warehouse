3e951b0b616aa854ef354525602607e81ce8fd21
Dual-Alen <a1320038493@163.com>
2021-01-06 15:53:51 +0800

    1.6 15:30 完善数据库设计

--- src/main/sql/database.sql
+++ src/main/sql/database.sql
@@ -39,14 +39,34 @@ CREATE TABLE `user_role` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
+-- goods 		货物**
+-- 字段 id、商品编号、商品名、规格型号、单位、总数、入库价格、出库价格、最低库存** 备注
+
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods` (
+  `id` int(32) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `num` varchar(64) DEFAULT NULL COMMENT '商品编号',
+  `name` varchar(64) DEFAULT NULL COMMENT '商品名',
+  `type` varchar(64) DEFAULT NULL COMMENT '商品类型',
+  `spe_model` varchar(64) DEFAULT NULL COMMENT '规格型号',
+  `unit` varchar(64) DEFAULT NULL COMMENT '单位',
+  `sum` double(64,0) DEFAULT NULL COMMENT '总数',
+  `in_price` double(64,0) DEFAULT NUll COMMENT '入库进价',
+  `out_price` double(64,0) DEFAULT NUll COMMENT '出库售价',
+  `cordon` double(64,0) DEFAULT NULL COMMENT '最低库存',
+  `note` varchar(128) DEFAULT NULL COMMENT '备注',
+
+                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
 
 -- order_list		订单详细信息**
-
--- 字段 id，商品名，订单编号，商品价格，商品数量，是否支付，下单时间、是否发货、物流信息、总金额、备注**
+-- 字段 id，商品名，商品编号、订单编号，商品价格，商品数量，是否支付，下单时间、是否发货、物流信息、总金额、备注**
 
 DROP TABLE IF EXISTS `order_list`;
 CREATE TABLE `order_list` (
   `id` int(32) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `num` varchar(64) DEFAULT NULL COMMENT '商品编号',
   `name` varchar(128) NOT NULL comment '商品名',
   `orderNum` varchar(128) DEFAULT NULL COMMENT '订单编号',
   `price` double(64,0) DEFAULT NULL COMMENT '商品价格',
@@ -54,7 +74,7 @@ CREATE TABLE `order_list` (
   `pay` tinyint(1) DEFAULT NULL COMMENT '是否支付',
   `date` datetime DEFAULT NULL COMMENT '下单时间',
   `transport` tinyint(1) DEFAULT NULL COMMENT '是否发货',
-  `orderState` int(32) DEFAULT '1' COMMENT '物流信息',
+  `orderState`varchar(128) DEFAULT NULL COMMENT '快递单号',
   `sum_price` double(64,0) DEFAULT NULL COMMENT '总金额',
   `note` varchar(128) DEFAULT NULL,
    PRIMARY KEY (`id`)
@@ -105,24 +125,6 @@ CREATE TABLE `client_order` (
   KEY `oid` (`oid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- goods 		货物**
--- 字段 id、商品编号、商品名、规格型号、单位、总数、入库价格、出库价格、最低库存** 备注
-
-DROP TABLE IF EXISTS `goods`;
-CREATE TABLE `goods` (
-  `id` int(32) NOT NULL AUTO_INCREMENT COMMENT '编号',
-  `num` varchar(64) DEFAULT NULL COMMENT '商品编号',
-  `name` varchar(64) DEFAULT NULL COMMENT '商品名',
-  `spe_model` varchar(64) DEFAULT NULL COMMENT '规格型号',
-  `unit` varchar(64) DEFAULT NULL COMMENT '单位',
-  `sum` double(64,0) DEFAULT NULL COMMENT '总数',
-  `in_price` double(64,0) DEFAULT NUll COMMENT '入库进价',
-  `out_price` double(64,0) DEFAULT NUll COMMENT '出库售价',
-  `cordon` double(64,0) DEFAULT NULL COMMENT '最低库存',
-  `note` varchar(128) DEFAULT NULL COMMENT '备注',
-
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- warehouse		仓库	字段：ID、仓库名、地址、容量、使用的容量、备注**
 
