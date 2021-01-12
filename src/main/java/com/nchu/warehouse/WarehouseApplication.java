82b59d866ba320e4cf3e04f969d32ecdfba968fc
Dual-Alen <a1320038493@163.com>
2021-01-12 11:11:55 +0800

    1.12 11:11 正式编写controller类

---
+++ src/main/java/com/nchu/warehouse/WarehouseApplication.java
@@ -0,0 +1,8 @@
+package com.nchu.warehouse;
+import org.springframework.boot.SpringApplication;
+
+public class WarehouseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseApplication.class, args);
+    }
+}
--- src/main/java/com/nchu/warehouse/controller/UserController.java
+++ src/main/java/com/nchu/warehouse/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.nchu.warehouse.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.nchu.warehouse.entity.User;
+import com.nchu.warehouse.service.UserService;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.HtmlUtils;
+
+import javax.security.auth.Subject;
+import javax.xml.transform.Result;
+
 
 /**
  * <p>
@@ -16,6 +23,96 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/warehouse/user")
 public class UserController {
+    @Autowired
+    UserService userService;
 
+    @PostMapping("/api/login")
+    public Result login(@RequestBody User requestUser) {
+        String username = requestUser.getName();
+        username = HtmlUtils.htmlEscape(username);
+
+        Subject subject = SecurityUtils.getSubject();
+//        subject.getSession().setTimeout(10000);
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username, requestUser.getPassword());
+        usernamePasswordToken.setRememberMe(true);
+        try {
+            subject.login(usernamePasswordToken);
+            User user = userService.findByUsername(username);
+            if (!user.isEnabled()) {
+                return ResultFactory.buildFailResult("该用户已被禁用");
+            }
+            return ResultFactory.buildSuccessResult(username);
+        } catch (IncorrectCredentialsException e) {
+            return ResultFactory.buildFailResult("密码错误");
+        } catch (UnknownAccountException e) {
+            return ResultFactory.buildFailResult("账号不存在");
+        }
+    }
+
+    @GetMapping("/api/logout")
+    public Result logout() {
+        Subject subject = SecurityUtils.getSubject();
+        subject.logout();
+        return ResultFactory.buildSuccessResult("成功登出");
+    }
+
+    @GetMapping("/api/authentication")
+    public String authentication() {
+        return "身份认证成功";
+    }
 }
 
+//public class LoginController {
+//
+//    @Autowired
+//    UserService userService;
+//
+//    @PostMapping("/api/login")
+//    public Result login(@RequestBody User requestUser) {
+//        String username = requestUser.getUsername();
+//        username = HtmlUtils.htmlEscape(username);
+//
+//        Subject subject = SecurityUtils.getSubject();
+////        subject.getSession().setTimeout(10000);
+//        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username, requestUser.getPassword());
+//        usernamePasswordToken.setRememberMe(true);
+//        try {
+//            subject.login(usernamePasswordToken);
+//            User user = userService.findByUsername(username);
+//            if (!user.isEnabled()) {
+//                return ResultFactory.buildFailResult("该用户已被禁用");
+//            }
+//            return ResultFactory.buildSuccessResult(username);
+//        } catch (IncorrectCredentialsException e) {
+//            return ResultFactory.buildFailResult("密码错误");
+//        } catch (UnknownAccountException e) {
+//            return ResultFactory.buildFailResult("账号不存在");
+//        }
+//    }
+//
+//    @PostMapping("/api/register")
+//    public Result register(@RequestBody User user) {
+//        int status = userService.register(user);
+//        switch (status) {
+//            case 0:
+//                return ResultFactory.buildFailResult("用户名和密码不能为空");
+//            case 1:
+//                return ResultFactory.buildSuccessResult("注册成功");
+//            case 2:
+//                return ResultFactory.buildFailResult("用户已存在");
+//        }
+//        return ResultFactory.buildFailResult("未知错误");
+//    }
+//
+//    @GetMapping("/api/logout")
+//    public Result logout() {
+//        Subject subject = SecurityUtils.getSubject();
+//        subject.logout();
+//        return ResultFactory.buildSuccessResult("成功登出");
+//    }
+//
+//    @GetMapping("/api/authentication")
+//    public String authentication() {
+//        return "身份认证成功";
+//    }
+//}
